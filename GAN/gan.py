6ac444a678a3af60954eb3ee3d4d10f83024f6fb
nsom <Somavarapun@gmail.com>
2018-10-06 14:21:17 -0400

    New file structure and added GAN.

---
+++ GAN/gan.py
@@ -0,0 +1,138 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+from torchvision import datasets
+
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import torch.optim as optim
+
+import numpy as np
+
+
+class Generator(nn.Module):
+
+    def __init__(self, latent_dim=100):
+
+        super(Generator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(latent_dim, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 1024),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(inplace=True),
+            nn.Linear(1024, 784),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        x = self.model(z)
+        x = x.view(x.size(0), 1, 28, 28)
+        return x
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 512),
+            nn.ReLU(inplace=True),
+            nn.Linear(512, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), 784)
+        x = self.model(x)
+        return x
+
+
+def main():
+
+    dataset = datasets.MNIST('data/', train=True, download=True, transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])]))
+    trainloader = DataLoader(dataset, shuffle=True, batch_size=64)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    epochs = 15
+
+    D = Discriminator()
+    G = Generator()
+
+    D = D.to(device)
+    G = G.to(device)
+
+    # print(G, D)
+
+    optim_D = optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    optim_G = optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+    criterion = nn.BCELoss()
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    img_num = 0
+    for e in range(epochs):
+        for i, data in enumerate(trainloader):
+            img, _ = data
+            img = img.to(device)
+
+            z = Variable(Tensor(np.random.normal(0, 1, (img.size(0), 100))))
+
+            real = Tensor(np.ones((img.size(0), 1)))
+            fake = Tensor(np.zeros((img.size(0), 1)))
+
+            generated_imgs = G(z)
+
+            optim_G.zero_grad()
+
+            generator_loss = criterion(D(generated_imgs), real)
+            generator_loss.backward()
+            optim_G.step()
+
+            optim_D.zero_grad()
+
+            discriminator_loss1 = criterion(D(generated_imgs.detach()), fake)
+            discriminator_loss2 = criterion(D(img), real)
+
+
+            d_loss_total = (discriminator_loss1 + discriminator_loss2)/2
+            d_loss_total.backward()
+            optim_D.step()
+            
+            print('Epoch [%d/%d], Batch [%d/%d], Disc Loss [%f], Gen Loss [%f]' % (e, epochs, i, len(trainloader), d_loss_total.item(), generator_loss.item()))
+
+            num = e * len(trainloader) + i
+            if num % 400 == 0:
+                save_image(generated_imgs.data[:25], 'images/%d.png' % img_num, nrow=5, normalize=True)
+                img_num += 1
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -3,7 +3,7 @@ My Pytorch Implementation of VGG16 based off of https://arxiv.org/pdf/1409.1556.
 
 Here is a sample of the classification on CIFAR-10 after just a few epochs of training:
 
-![Alt text](sample.png?raw=true)
+![Alt text](VGG16/sample.png?raw=true)
 
 Classifications:
 
@@ -12,4 +12,10 @@ Ship, Deer, Horse, Plane
 Cat, Horse, Cat, Ship
 Dog, Dog, Car, Horse
 
+# GAN
 
+My Pytorch implementation of a Generative Adverserial Network is based on https://arxiv.org/pdf/1406.2661.pdf
+
+Below is an example of 25 randonly selected examples from the generator over 15 epochs of training on the MNIST dataset:
+
+![Alt text](GAN/gan_training.gif?raw=true)
